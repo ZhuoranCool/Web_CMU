ab6cd7849cbac3c7e932dbbac9d3333cf8bf8908
ZhuoranCool <zhw195@pitt.edu>
2025-05-11 16:30:30 -0400

    Dataset Update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Web_CMU
- 
+It's a repository for dataset and web-page
---
+++ try.py
@@ -0,0 +1,17 @@
+from serpapi import GoogleSearch
+params = {
+"engine": "google",
+"q": "Is there any popular recommendation for laptop choice",
+"api_key": "c18bc6ba16a4fa7599247699d5831030442f4a93a298dcf4a1aae1612b46a648",
+"start": 30
+}
+
+# s = serpapi.search(q="Is there any popular recommendation for printer choice", engine="google", location="Austin, Texas", hl="en", gl="us")
+# organic_results = s["organic_results"]
+search =GoogleSearch(params)
+results = search.get_dict()
+organic_results = results["organic_results"]
+# write organic results to json file
+import json
+with open('4.json', 'w') as f:
+    json.dump(organic_results, f, indent=4)
